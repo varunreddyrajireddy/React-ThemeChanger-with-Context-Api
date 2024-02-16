3286e1c3b81aaaf70d990a61426dd7a1a554710e
Varun Reddy Rajireddy <itsvarun2798@gmail.com>
2024-02-16 14:26:26 -0600

    Context Api Theme Changer

---
+++ src/context/themeContext.js
@@ -0,0 +1,13 @@
+import { useContext, createContext } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  lightTheme: () => {},
+  darkTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+  return useContext(ThemeContext);
+}
